7f026affb78829eea6bf918f31807f2d8b1f4929
iam9 <ibrahim.asif.mirza@gmail.com>
2018-10-31 16:25:51 +0100

    .

--- ExpressApp2_test/ExpressApp2_test/app.js
+++ ExpressApp2_test/ExpressApp2_test/app.js
@@ -12,7 +12,7 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
+// view engine setups
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
