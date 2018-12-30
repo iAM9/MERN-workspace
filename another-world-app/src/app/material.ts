785bd70a7b9fdab767193b535ee26a4f9634f14f
iam9 <ibrahim.asif.mirza@gmail.com>
2018-12-30 15:32:46 +0100

    .

--- another-world-app/src/app/material.ts
+++
@@ -1,8 +0,0 @@
-import {MatButtonModule, MatButtonToggleModule} from '@angular/material';
-import { NgModule } from '@angular/core';
-
-@NgModule({
-  imports: [MatButtonModule, MatButtonToggleModule],
-  exports: [MatButtonModule, MatButtonToggleModule],
-})
-export class materialModule { }
